6b37ed2201174caa86a44e8ac3350dd09e91e606
R Primus <rprimus@users.noreply.github.com>
2019-07-01 19:00:18 +0100

    * sly-named-readtables-autoloads.el: Generate with package-generate-autoloads

--- sly-named-readtables-autoloads.el
+++ sly-named-readtables-autoloads.el
@@ -1,10 +1,24 @@
+;;; sly-named-readtables-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
 ;;;### (autoloads nil "sly-named-readtables" "sly-named-readtables.el"
-;;;;;;  (21964 37122 0 0))
+;;;;;;  (0 0 0 0))
 ;;; Generated autoloads from sly-named-readtables.el
 
 (add-to-list 'sly-contribs 'sly-named-readtables 'append)
 
-;;;***
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "sly-named-readtables" '("sly-named-readtable")))
 
-(provide 'sly-named-readtables-autoloads)
+;;;***
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sly-named-readtables-autoloads.el ends here
